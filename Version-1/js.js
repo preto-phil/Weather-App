d7c3287baee6080bd1b0cde43af29daa594e5c18
Philip Pretorius <philippretorius.79@gmail.com>
2023-06-03 14:01:39 +0200

    Burned wood and added comments for clarity

--- Version-1/js.js
+++ Version-1/js.js
@@ -1,20 +1,23 @@
-/* 
-Weather App
+/*  Weather App
 
 Weather API: 'https://api.weatherapi.com/v1/current.json?key=e0d3c7ebf93b4fb48c1121421233105&q=thabazimbi'
 
 */
 
-// Variables
-
-
-
+// Set initial / default search input value
 let searchInputValue = 'Thabazimbi';
+
+// Get search button element
 const searchBtn = document.querySelector('#search-btn');
+
+// Add event listener to search button
 searchBtn.addEventListener('click', () => {
+  // check if search input is empty
   if (document.querySelector('#search').value === '') {
+    // Use default search input value
     getWeather();
   } else {
+    // Use the entered search input value, whilst trimming whitespace
     const rawSearchValue = document.querySelector('#search').value;
     searchInputValue = encodeURIComponent(rawSearchValue.trim());
     getWeather();
@@ -22,69 +25,33 @@ searchBtn.addEventListener('click', () => {
 });
 
 
-document.querySelector('input').value;
-
-
-// Function that fetches weather api
-
+// Function that fetches weather data
 async function getWeather() {
-
   try {
-
+    // Fetch weather data from API
     let response = await fetch(`https://api.weatherapi.com/v1/current.json?key=e0d3c7ebf93b4fb48c1121421233105&q=${searchInputValue}`, { mode: 'cors' });
     let result = await response.json();
-
+    // Call parseData function to extract and process some weatherData
     parseData(result);
     console.log(result);
     return result;
-
   } catch (err) {
-
     throw new Error(`Oops! ${err}`)
-
   }
 }
 
-async function parseData(result) {
-  let object = {
+
+// Function to parse weather data
+function parseData(result) {
+  // Extract necessary weather data
+  const weatherData = {
   condition: result.current.condition.text,
   temperature_c: result.current.temp_c,    
   }
-  console.log(object)
+  console.log(weatherData)
+  return weatherData;
 }
 
-/* Function that processes JSON data returned from getWeather()
-
-*/
-
-
-/* 
-
-parseData(getWeather());
- */
-/* 
-getWeather().then(() => {
-  let object = {
-    condition: result.data.current.condition.text,
-
-  }
-  console.log(object)
-  return object;
-}); */
-
-
-
-
-/*  Create JSON object with the result from getWeather() 
-
-// Stringify?
-// Parse?
-
-// condition = result.data.current.condition.text
-// cloud = result.data.current.loud
-// etc..
-
-*/
 
 /* Data I want to process
 
