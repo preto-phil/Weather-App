389a0c76de54747a649196518a58741253499972
Philip Pretorius <philippretorius.79@gmail.com>
2023-06-05 21:41:57 +0200

    Minor styling + Arrow slider code added

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Weather-App
 The Odin Project Javascript: Weather App
+
+<!-- # 
+
+What I'm learning and using: 
+
+- Working with APIs
+- Asynchronous code
+-- Promises, Async/await
+- Dynamic User Interface Interaction
+- Object manipulation
+
+
+# Screenshot
+
+- add screenshot -->
--- Version-1/js.js
+++ Version-1/js.js
@@ -236,3 +236,39 @@ function createHourlyDivs(hourlyData) {
 }
 
 
+/* 
+DUII - Arrow slider
+*/
+
+/* let currentIndex = 1;
+displaySlides(currentIndex);
+
+function setSlides(num) {
+  displaySlides(currentIndex += num);
+}
+
+function timerSlide() {
+  setInterval(() => {
+    setSlides(1);
+  }, '5000')
+}
+
+timerSlide();
+
+
+function displaySlides(num) {
+  let x;
+  const slides = document.getElementsByClassName('imageSlides');
+  if (num > slides.length) { currentIndex = 1 }
+  if (num < 1) { currentIndex = slides.length }
+  for (x = 0; x < slides.length; x++) {
+    slides[x].style.display = 'none';
+  }
+  slides[currentIndex - 1].style.display = 'block';
+
+  const dots = document.getElementsByClassName("dot");
+  for (x = 0; x < dots.length; x++) {
+    dots[x].className = dots[x].className.replace(" active", "");
+  }
+  dots[currentIndex - 1].className += " active";
+} */
