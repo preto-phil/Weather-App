6f2ef0bbab8119126938bc17c0a395baa129351e
Philip Pretorius <philippretorius.79@gmail.com>
2023-06-07 23:16:25 +0200

    Footer added with github icon functionality

--- Version-1/js.js
+++ Version-1/js.js
@@ -405,3 +405,13 @@ windButton.addEventListener('click', () => {
   toggleWind();
   toggleWindDiv();
 });
+
+
+function openGithub() {
+  const githubIcon = document.getElementById('github-icon');
+  githubIcon.addEventListener('click', () => {
+    window.open('https://github.com/preto-phil');
+  });
+}
+
+openGithub();
