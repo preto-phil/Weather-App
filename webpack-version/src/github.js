ea97148c2ea766b1bf53654aabe2498edf1bc451
Philip Pretorius <philippretorius.79@gmail.com>
2023-06-14 18:40:19 +0200

    Webpack version removed

--- webpack-version/src/github.js
+++
@@ -1,6 +0,0 @@
-export default function openGithub() {
-  const githubIcon = document.getElementById('github-icon');
-  githubIcon.addEventListener('click', () => {
-    window.open('https://github.com/preto-phil');
-  });
-}
